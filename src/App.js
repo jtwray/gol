40f4c825f9bab3ffd8137b774a6d606ac3367202
James 'Tucker' Wray <tuckerwray84@gmail.com>
2020-09-04 14:21:15 -0400

    Extracts Controls into its own component Grid&Controls  - Compartmentalizes Grid and Controls into separate components.  - updateGrid function and   populated2dArray state Hook lifted up into App.js  -Lifting them up allows Grid and Controls to access  both functions as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { useState, useEffect } from "react";
 import Generations from "./Generations.js";
+import Controls from "./Controls"
 import Grid from "./Grid.js";
-
+let tipURL=  'https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#cite_note-1';
 export default function App() {
+  
   const [gen, setGen] = useState(0);
+  let [populated2dArray, setPopulated2dArray] = useState();
+  function updateBox(grid, c, r) {
+    console.log(populated2dArray[c][r])
+    grid[c][r] == 1 ? (grid[c][r] = 0) : (grid[c][r] = 1);
+
+  }
   return (
     <>
-      {/* <Generations gen={gen} setGen={setGen} /> */}
-      <Grid gen={gen} setGen={setGen} style={{ background:"purple" }} />
+      <section>
+        <h1>The Game of Life</h1>
+        <h2>
+          <p>
+            The Game of Life, also known simply as Life, is a cellular automaton
+            devised by the British mathematician John Horton Conway in 1970.
+            <span className="tooltip">[1]
+              <span className="tooltiptext">
+               https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#cite_note-1
+              </span>
+            </span>
+            It is a zero-player game, meaning that its evolution is determined
+            by its initial state, requiring no further input. It is Turing
+            complete and can simulate a universal constructor or any other
+            Turing machine. One interacts with the Game of Life by creating an
+            initial configuration and observing how it evolves.
+          </p>
+        </h2>
+<Controls  updateBox={updateBox} gen={gen} setGen={setGen} populated2dArray={populated2dArray} setPopulated2dArray={setPopulated2dArray}/>
+      </section>
+
+      <section>
+        <Grid updateBox={updateBox} gen={gen} setGen={setGen} style={{ background: "purple" }} populated2dArray={populated2dArray} setPopulated2dArray={setPopulated2dArray}/>
+      </section>
     </>
   );
 }
--- src/Box.js
+++ src/Box.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-export default function Box({box}) {
+export default function Box({box,gridSize,coli,ri,populated2dArray,updateBox}) {
+
+let boxWidth=100/gridSize
+
   
   return (
     <div
       style={{
         display: "inline-block",
         textAlign: "center",
-        width: "5%",
+        width: `${gridSize}`,
         height: "100%",
         color: "grey",
         background: "yellow",
         border: "black solid 1px",
-        ...((box === 1 && { color: "white", background: "black" }) ||
-          (box === 0 && { color: "black" }))
+        ...((box === 1 && { color: "yellow", background:  `black` }) ||
+          (box === 0 && { color: `black` }))
       }}
-  
+  onClick={()=>{updateBox(populated2dArray,coli,ri)}}
     >
       {box}
     </div>
