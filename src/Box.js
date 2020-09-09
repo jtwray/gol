205b751829d0797f69dc2733c2a22be1300fb81f
tucker <tuckerwray84@gmail.com>
2020-09-09 19:54:15 -0400

    Refactor Box.js Comments out the Ref, BoxIsAlive state, effect Hook , CheckBoxColor function less code less bugs! these are all now handled with the updateBox function and the inline styles

--- src/Box.js
+++ src/Box.js
@@ -10,34 +10,34 @@ export default function Box({
   updateBox
 }) {
   let boxWidth = 100 / gridSize;
-  const boxRef = React.useRef();
-  const [boxIsAlive, setBoxIsAlive] = useState(box?true:false);
+  // const boxRef = React.useRef();
+  // const [boxIsAlive, setBoxIsAlive] = useState(box?true:false);
 
-  function checkBoxColor(box) {
+  // function checkBoxColor(box) {
 
-    if (boxIsAlive) {
-      boxRef.current.classList.add("liveCell");
+  //   if (boxIsAlive) {
+  //     boxRef.current.classList.add("liveCell");
 
-      box=1;
-    }
-    if (!boxIsAlive) {
-      boxRef.current.classList.remove("liveCell");
-    }
-  }
-  useEffect(() => {
+  //     box=1;
+  //   }
+  //   if (!boxIsAlive) {
+  //     boxRef.current.classList.remove("liveCell");
+  //   }
+  // }
+  // useEffect(() => {
 
-    if (boxIsAlive) {
-      boxRef.current.classList.add("liveCell");
-      console.log("livebox")
-    }    
-    if (!boxIsAlive) {
-      boxRef.current.classList.remove("liveCell");
-    }
-  }, [box, boxIsAlive]);
+  //   if (boxIsAlive) {
+  //     boxRef.current.classList.add("liveCell");
+  //     console.log("livebox")
+  //   }    
+  //   if (!boxIsAlive) {
+  //     boxRef.current.classList.remove("liveCell");
+  //   }
+  // }, [box, boxIsAlive]);
 
   return (
     <div
-      ref={boxRef}
+      // ref={boxRef}
       className="box"
       style={{
         width: `${gridSize}`,
@@ -46,7 +46,7 @@ export default function Box({
       }}
       onClick={() => {
         updateBox(populated2dArray, ri,coli);
-        setBoxIsAlive(!boxIsAlive);
+        // setBoxIsAlive(!boxIsAlive);
       }}
     >
       {box}
