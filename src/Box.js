288b4d9bd01332261573be3add8f5d1b17f4ded9
tucker <tuckerwray84@gmail.com>
2020-09-09 20:11:10 -0400

    Removes COnsole.log from Box.js centers int vale of box cells across both axises with flexbox

--- src/Box.js
+++ src/Box.js
@@ -10,43 +10,18 @@ export default function Box({
   updateBox
 }) {
   let boxWidth = 100 / gridSize;
-  // const boxRef = React.useRef();
-  // const [boxIsAlive, setBoxIsAlive] = useState(box?true:false);
-
-  // function checkBoxColor(box) {
-
-  //   if (boxIsAlive) {
-  //     boxRef.current.classList.add("liveCell");
-
-  //     box=1;
-  //   }
-  //   if (!boxIsAlive) {
-  //     boxRef.current.classList.remove("liveCell");
-  //   }
-  // }
-  // useEffect(() => {
-
-  //   if (boxIsAlive) {
-  //     boxRef.current.classList.add("liveCell");
-  //     console.log("livebox")
-  //   }    
-  //   if (!boxIsAlive) {
-  //     boxRef.current.classList.remove("liveCell");
-  //   }
-  // }, [box, boxIsAlive]);
-
+let fontSize=`${boxWidth*20}%`;
   return (
     <div
-      // ref={boxRef}
       className="box"
-      style={{
-        width: `${gridSize}`,
+      style={{fontSize,
+        width: `${boxWidth}`,
         ...(box===1&&{background:'black',color:'yellow'}),
         ...(box===0&&{background:'yellow',color:'black'})
       }}
       onClick={() => {
         updateBox(populated2dArray, ri,coli);
-        // setBoxIsAlive(!boxIsAlive);
+
       }}
     >
       {box}
