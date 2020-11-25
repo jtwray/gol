1dbf15675cbf682c1defaec7fdcf062b0012686b
James 'Tucker' Wray <tuckerwray84@gmail.com>
2020-11-24 23:56:23 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,25 +6,19 @@ import Generations from "./Generations.js";
 import Controls from "./controls/Controls";
 import Grid from "./Grid.js";
 import Row from "./Row.js";
-let tipURL =
-  "https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#cite_note-1";
 
 export default function App() {
   const [gen, setGen] = useState(0);
-
-  let [populated2dArray, setPopulated2dArray] = useState();
+  const [populated2dArray, setPopulated2dArray] = useState();
   const [gridSize, setGridSize] = useState(50);
 
   function updateBox(grid, r, c) {
-    console.log(populated2dArray[r][c], { r }, { c });
     grid[r][c] === 1 ? (grid[r][c] = 0) : (grid[r][c] = 1);
     let updatedCell = grid[r][c];
-
-    console.log({ updatedCell }, "updatedCell", updatedCell);
     setPopulated2dArray([
       ...populated2dArray.map((row) => {
         let newRow = [...row];
-        if (row != r) {
+        if (row !== r) {
           return [...newRow];
         } else {
           return [...newRow, (row[c] = updatedCell)];
@@ -35,16 +29,23 @@ export default function App() {
 
   return (
     <>
+      <h1>The Game of Life</h1>
       <nav>
         <Link to="/">LIFE</Link>
         <Link to="/rules">RULES</Link>
         <Link to="/about">ABOUT</Link>
       </nav>
-      <Route exact path="/" component={Grid} />
-      <Route exact path="/rules" component={Rules} />
-      <Route exact path="/about" component={About} />
-      <h1>The Game of Life</h1>
-      <section className="about">
+
+      <Route exact path="/">
+        <Grid
+          updateBox={updateBox}
+          gen={gen}
+          setGen={setGen}
+          gridSize={gridSize}
+          setGridSize={setGridSize}
+          populated2dArray={populated2dArray}
+          setPopulated2dArray={setPopulated2dArray}
+        />{" "}
         <Controls
           className="section--Controls"
           updateBox={updateBox}
@@ -55,19 +56,9 @@ export default function App() {
           populated2dArray={populated2dArray}
           setPopulated2dArray={setPopulated2dArray}
         />
-      </section>
-
-      <section className="section--Grid">
-        <Grid
-          updateBox={updateBox}
-          gen={gen}
-          setGen={setGen}
-          gridSize={gridSize}
-          setGridSize={setGridSize}
-          populated2dArray={populated2dArray}
-          setPopulated2dArray={setPopulated2dArray}
-        />
-      </section>
+      </Route>
+      <Route exact path="/rules" component={Rules} />
+      <Route exact path="/about" component={About} />
     </>
   );
 }
